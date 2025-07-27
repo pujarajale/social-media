23e08c9404721189af17d66239ecd7ff154660a6
pujarajale <rajalepuja1997@gmail.com>
2025-07-27 10:40:57 +0530

    Layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import PostList from "./components/PostList";
+import CreatePost from "./components/CreatePost";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <div className="app-container">
+        <Sidebar></Sidebar>
+        <div className="content">
+          <Header></Header>
+          <CreatePost></CreatePost>
+          <PostList></PostList>
+          <Footer></Footer>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
